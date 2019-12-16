1cb1a1dbf712ed70c6b797773cef5ca4a77382f5
TonyBarkell <tonybarkell@gmail.com>
2019-12-16 13:46:36 +0000

    initial commit

--- public/app/scripts/main.js
+++ public/app/scripts/main.js
@@ -1,4 +1,40 @@
+/*
+    Example code demostrating how to have a window transisition from 50% Opcaity to 100%
+    once the DOM content is loaded (The Window Opacity needs to be set to .5 (or other) 
+    in the application configuration, or window options, waitForPageLoad should be 'false')
+*/
 
+// First add a Lidterner for the DOMContentLoaded event
+document.addEventListener('DOMContentLoaded', () => {
+    // The 2 second setTimeout here is used to simulate a slow load time
+    // In prodiction code the function should not be wrapped in a setTimeout call
+    setTimeout(setFullOpacity().then(() => console.log('opacity updated')).catch(err => console.error(err)), 2000);
+});
+
+// This function is called when the 'DOMContentLoaded' event is fired
+// It uses the OpenFin API animate to transition from the windows current opcity state, to full opacity over 500ms
+async function setFullOpacity() {
+    // Set the animation to change the window opcaity to '1' (full opacity) over 500ms
+    const transitions = {
+        opacity: {
+            opacity: 1,
+            duration: 500
+        }
+    };
+    const options = {
+        interrupt: true,
+        tween: 'ease-in'
+    };
+
+    // Get a reference to the current window and call animate
+    // More inframtion on animate can be found at https://developer.openfin.co/docs/javascript/stable/tutorial-Window.animate.html
+    const win = await fin.Window.getCurrent();
+    return win.animate(transitions, options);
+};
+
+/*
+    Generic Helper code
+*/
 
 function minimizeWindow(){
     var finWindow = fin.Window.getCurrentSync();
@@ -20,11 +56,6 @@ function closeWindow(){
     finWindow.close().then(() => console.log('Window closed')).catch(err => console.log(err));
 }
 
-// Requires the Layouts v1 API to be imported, or built into the project
-function undockWindow(){
-    layouts.snapAndDock.undockWindow();
-}
-
 function openChildWindow(){
     var serverPort;
     fin.Window.getCurrentSync().getOptions()
@@ -38,6 +69,8 @@ function openChildWindow(){
             defaultWidth: 300,
             defaultHeight: 300,
             saveWindowState : false,
+            waitForPageLoad : false,
+            opacity : .5,
             url: url,
             frame: false,
             autoShow: true
@@ -47,15 +80,13 @@ function openChildWindow(){
             console.log("window created")
         );
     }).catch(err => console.log(err));
-}
+};
 
-//Once the DOM has loaded and the OpenFin API is ready
 function onMain() {
     console.log("on main");
-        fin.Window.getCurrentSync().getOptions()
-            .then(opts => {
-                serverPort = opts.customData;
-                console.log("Server Port: " + serverPort);
-            }).catch(err => console.log(err));
+    fin.Window.getCurrentSync().getOptions().then(opts => {
+        serverPort = opts.customData;
+        console.log("Server Port: " + serverPort);
+    }).catch(err => console.log(err));
 };
 
